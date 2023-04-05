d8529969a0aab21b007e2caa87d1f5a90f4426ba
Lukas2380 <96386473+Lukas2380@users.noreply.github.com>
2023-04-05 13:46:44 +0200

    changed directory because of a mistake

---
+++ Data/Song.cs
@@ -0,0 +1,81 @@
+﻿using Common.NotifyPropertyChanged;
+using System.Windows.Media.Imaging;
+
+namespace Data
+{
+    public class Song : NotifyPropertyChanged
+    {
+        #region Fields
+
+        private string title;
+        private string albumName;
+        private BitmapImage albumCover;
+
+        #endregion
+
+        #region Constructor
+
+        public Song(string title, string albumName, BitmapImage albumCover)
+        {
+            this.title = title;
+            this.albumName = albumName;
+            this.albumCover = albumCover;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (title != value)
+                {
+                    title = value;
+                    OnPropertyChanged(nameof(this.title));
+                }
+            }
+        }
+
+        public string AlbumName
+        {
+            get => albumName;
+            set
+            {
+                if (albumName != value)
+                {
+                    albumName = value;
+                    OnPropertyChanged(nameof(this.albumName));
+                }
+            }
+        }
+
+        public BitmapImage AlbumCover
+        {
+            get => albumCover;
+            set
+            {
+                if (albumCover != value)
+                {
+                    albumCover = value;
+                    OnPropertyChanged(nameof(this.albumCover));
+                }
+            }
+        }
+
+
+        #endregion
+
+        #region ------------------------- Private helper ------------------------------------------------------------------
+        //public override string ToString()
+        //{
+        //    return this.Title;
+        //}
+        #endregion
+
+        #region ------------------------- Commands ------------------------------------------------------------------------
+        #endregion
+    }
+}
